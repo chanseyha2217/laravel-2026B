0e430e95775eab0d240476c8f744bf87953c555d
yen yon <yonyen99@gmail.com>
2026-06-04 08:19:21 +0700

    project structure 2026B

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        // raw sql
+        // $categories = DB::select('SELECT * FROM categories');
+        // Query Builder 
+        // $categories = DB::table('categories')->get();
+        // Eloquent ORM:
+        $categories = Category::orderBy('id', 'desc')->get();
+        // dd($categories);
+        return view('categories.list', compact('categories'));
+    }
+
+    public function create()
+    {
+        // dd(112);
+        return view('categories.create');
+    }
+
+    public function store()
+    {
+        // dd(111);
+        // dd(request()->all());
+        Category::create(
+            [
+                'name' => request()->name,
+                'dec' => request()->dec,
+            ]
+        );
+        // return view('categories.list'); don't like this
+        return redirect('/categories');
+    }
+}
---
+++ resources/views/categories/list.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        {{-- {{dd($categories)}} --}}
+        <a href="{{route('categories.create')}}" class="btn btn-info"> create+</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Des</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $index => $category)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ $category->dec }}</td>
+                        <td>
+                            <a href="" data-bs-toggle="modal" data-bs-target="#showCategory{{$category->id}}">
+                                view
+                            </a>
+                            <!-- Modal -->
+                            @include('categories.show')
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
