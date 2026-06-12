3b6f425d71287c2f98103dba552f3759b4f6e764
chanseyha2217 <chanseyha.horurn2217@gmail.com>
2026-06-12 09:04:36 +0700

    add route API

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 
-Route::apiResource('categories', CategoryController::class);
+Route::apiResource('categories', CategoryController::class)->names([
+    'index'   => 'api.categories.index',
+    'store'   => 'api.categories.store',
+    'show'    => 'api.categories.show',
+    'update'  => 'api.categories.update',
+    'destroy' => 'api.categories.destroy',
+]);
